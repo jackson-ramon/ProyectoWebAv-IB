8a4ef28f2a3a7c7c9f9eb1788b7f8bfc8aefdec0
KEVIN CONDOR <kevin.condor@epn.edu.ec>
2024-06-17 17:26:09 -0500

    chore: Add SSL configuration for database connection, and add a template for env

--- mypet-back/db/data-source.ts
+++ mypet-back/db/data-source.ts
@@ -11,7 +11,10 @@ export const dataSourceOptions:DataSourceOptions={
     entities:['dist/**/*.entity{.ts,.js}'],
     migrations:['dist/db/migrations/*{.ts,.js}'],
     logging: false,
-    synchronize: false
+    synchronize: false,
+    ssl: {
+        rejectUnauthorized: false
+    },
 }
 
 const dataSource = new DataSource(dataSourceOptions);
---
+++ mypet-back/db/migrations/1718662079680-initial.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Initial1718662079680 implements MigrationInterface {
+    name = 'Initial1718662079680'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "products" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "price" integer NOT NULL, "imageUrl" character varying, "deletedAt" TIMESTAMP, "userId" integer, CONSTRAINT "pk_product_id" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "pk_user_id" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`ALTER TABLE "products" ADD CONSTRAINT "FK_99d90c2a483d79f3b627fb1d5e9" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "products" DROP CONSTRAINT "FK_99d90c2a483d79f3b627fb1d5e9"`);
+        await queryRunner.query(`DROP TABLE "users"`);
+        await queryRunner.query(`DROP TABLE "products"`);
+    }
+
+}
